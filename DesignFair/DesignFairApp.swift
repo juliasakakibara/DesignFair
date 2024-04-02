3bf34231e4ec9b04e1d63001634762bcc278e28d
juliasakakibara <juliasakakibara.o@gmail.com>
2024-04-02 17:20:02 -0300

    Initial Commit

---
+++ DesignFair/DesignFairApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignFairApp.swift
+//  DesignFair
+//
+//  Created by Julia Sakakibara on 02/04/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignFairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
